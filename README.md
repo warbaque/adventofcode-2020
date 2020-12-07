57e60f5606ae51d02013f3df4fb8e8716cb00b59
Klaus Helenius <klaus.helenius@gmail.com>
2020-12-07 19:23:32 +0200

    Add helpers and README

---
+++ README.md
@@ -0,0 +1,13 @@
+# [Advent of Code 2020](https://adventofcode.com/2020) solutions (Python)
+
+Example usage
+```sh
+❯ helpers/get-input 3
+❯ ./solutions.py solver day3
+```
+
+`helpers/.env` for `helpers/get-input`
+```sh
+SESSION_TOKEN=<AOC SESSION TOKEN>
+```
+
--- solutions.py
+++ solutions.py
@@ -12,7 +12,7 @@ inputs = dict((f"day{i+1}", f"inputs/{i+1}") for i in range(25))
 # https://adventofcode.com/2020/day/1
 def day1(input):
     numbers = {int(i) for i in input.split()}
-    n = sorted(numbers)
+    n = sorted(numbers) # speeds up quadratic solution
 
     print(next(x * (2020-x) for x in numbers if 2020-x in numbers))
     print(next(x * y * (2020-x-y) for x in n for y in n if 2020-x-y in numbers))
@@ -22,12 +22,14 @@ def day1(input):
 def day2(input):
     profiles = input.split('\n')
     r = re.compile(r'(\d+)-(\d+) (\S): (\S+)')
+
     count_part1 = 0
     count_part2 = 0
     for password_profile in profiles:
         lo, hi, c, pw = r.match(password_profile).groups()
         count_part1 += (int(lo) <= pw.count(c) <= int(hi))
         count_part2 += (pw[int(lo)-1] == c) ^ (pw[int(hi)-1] == c)
+
     print(count_part1)
     print(count_part2)
 
