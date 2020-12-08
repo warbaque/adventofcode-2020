261d8097ce5206c1c36deec5afe2e510fe28300f
Klaus Helenius <klaus.helenius@gmail.com>
2020-12-08 19:05:30 +0200

    Solve day 8

--- solutions.py
+++ solutions.py
@@ -130,6 +130,70 @@ def day7(input):
     print(count_children('shiny gold'))
 
 
+# https://adventofcode.com/2020/day/8
+def day8(input):
+    instructions = input.strip().split('\n')
+
+    r = re.compile(r'(?P<op>\S+) (?P<arg>[+-]\d+)')
+
+    def make_instruction(match):
+        return (
+            match['op'],
+            int(match['arg'])
+        )
+
+    program = [make_instruction(r.match(i)) for i in instructions]
+
+    def acc(program_state, x):
+        program_state['accumulator'] += x
+        program_state['instruction'] += 1
+
+    def nop(program_state, _):
+        program_state['instruction'] += 1
+
+    def jmp(program_state, x):
+        program_state['instruction'] += x
+
+    def run_until_loop(program):
+        program_state = {
+            'instruction': 0,
+            'accumulator': 0,
+            'done': False,
+        }
+
+        visited = set()
+        while program_state['instruction'] not in visited and not program_state['done']:
+            instruction = program_state['instruction']
+            visited.add(instruction)
+
+            op, arg = program[instruction]
+            {
+                'acc': acc,
+                'nop': nop,
+                'jmp': jmp,
+            }[op](program_state, arg)
+            program_state['done'] = program_state['instruction'] >= len(program)
+
+        return program_state
+
+    def alternate_programs(program):
+        swaps = {'jmp': 'nop', 'nop': 'jmp'}
+        for i, instruction in enumerate(program):
+            op, arg = instruction
+            if op in swaps:
+                new_program = program.copy()
+                new_program[i] = (swaps[op], arg)
+                yield new_program
+
+    print(run_until_loop(program)['accumulator'])
+    print(next(
+        program_state['accumulator']
+            for program_state in (
+                run_until_loop(p) for p in alternate_programs(program)
+            ) if program_state['done']
+        ))
+
+
 def solver(day):
     with open(inputs[day], "r") as f:
         globals()[day](f.read())
